874df562cc34bd47434e563b1ae32eb29eebb947
arkalel <arkalel2004@gmail.com>
2026-05-19 12:38:50 +0100

    Update: modified next_state.py, added images and new files

--- week_1/next_state_difference.py
+++ week_1/next_state_difference.py
@@ -12,16 +12,16 @@ x_cart_velocity = []
 x_pole_angle = []
 x_pole_velocity = []
 Y = []
-y_cart_position = []
-y_cart_velocity = []
-y_pole_angle = []
-y_pole_velocity = []
+d_cart_position = []
+d_cart_velocity = []
+d_pole_angle = []
+d_pole_velocity = []
 
 for i in range(500):
     example_system = CartPole(visual=False)
     cart_position = random.uniform(-2.5, 2.5)
     cart_velocity = random.uniform(-10, 10)
-    pole_angle = random.uniform(-np.pi, np.pi)
+    pole_angle = -1
     pole_velocity = random.uniform(-15, 15)
 
     state = [cart_position, cart_velocity, pole_angle, pole_velocity]
@@ -35,15 +35,18 @@ for i in range(500):
     example_system.performAction()
 
     current_state = example_system.getState()
-    Y.append(current_state)
-    y_cart_position.append(current_state[0])
-    y_cart_velocity.append(current_state[1])
-    y_pole_angle.append(current_state[2])
-    y_pole_velocity.append(current_state[3])
+    Y.append(current_state - state)
+    d_cart_position.append(current_state[0] - state[0])
+    d_cart_velocity.append(current_state[1] - state[1])
+    d_pole_angle.append(current_state[2] - state[2])
+    d_pole_velocity.append(current_state[3] - state[3])
 print(X)
 print(Y) 
 plt.figure()   
-plt.scatter(x_cart_position, y_cart_position, label='cart position at t+1 vs cart position at t')
-plt.ylabel('Next cart position')
-plt.xlabel('Previous cart position')
+plt.tricontourf(x_pole_velocity, x_cart_velocity, d_cart_position, levels=50, cmap='viridis')
+plt.colorbar(label='Change in Cart Position')
+#plt.scatter(x_pole_velocity, d_cart_position, label='change in cart position vs pole velocity at t')
+plt.xlabel('Pole Velocity')
+
+plt.ylabel('Cart Velocity')
 plt.show()
